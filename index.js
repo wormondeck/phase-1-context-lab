40adee1e9411cbee879c5caeddbef85ddb897a22
Ronney Arias <ronneyarias@gmail.com>
2023-11-15 21:37:18 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
-/* Your Code Here */
+function createEmployeeRecord(employee) {
+    
+    return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(e) {
+   
+    let [date, hour] = e.split(" ");
+    let eventObj = {
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date
+   };
+   this.timeInEvents.push(eventObj)
+   return this
+}
+
+function createTimeOutEvent(e) {
+    
+    let [date, hour] = e.split(" ");
+    let eventObj = {
+     type: "TimeOut",
+     hour: parseInt(hour, 10),
+     date
+    };
+    this.timeOutEvents.push(eventObj)
+    return this
+ }
+
+ function hoursWorkedOnDate(d) {
+    let timeIn = this.timeInEvents.find(e => e.date === d)
+    let timeOut = this.timeOutEvents.find(e => e.date === d)
+
+    return (timeOut.hour - timeIn.hour)/100
+ }
+
+ function wagesEarnedOnDate(d) {
+    let hours = hoursWorkedOnDate.call(this, d)
+    return this.payPerHour * hours
+}
+
+function calculatePayroll(employeeRecords) {
+    
+    return employeeRecords.map(employee => allWagesFor.call(employee)).reduce((currentValue, total) => currentValue + total)
+}
+
+function findEmployeeByFirstName(employees, firstNameString) {
+    const found = employees.find(emp => emp.firstName === firstNameString)
+    return found
+} 
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -14,8 +76,8 @@ const allWagesFor = function () {
         return e.date
     })
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
+    const payable = eligibleDates.reduce(function (m, d) {
+        return m + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
